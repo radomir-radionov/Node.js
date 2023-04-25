d84ceafd5c8c5664b7be5d6302dc565e098d91a3
Your Name <you@example.com>
2023-04-25 19:18:29 +0300

    feat: package

---
+++ README.md
@@ -0,0 +1,11 @@
+import package
+
+```javascript
+import calc from 'package_radomir_dependecies';
+```
+
+use
+
+```javascript
+console.log(calc(5, 6));
+```
---
+++ index.mjs
@@ -0,0 +1,7 @@
+export const logEnvironment = () => {
+  return console.log(process.env.Node_ENV);
+};
+
+export const calc = (a, b) => {
+  return a + b;
+};
