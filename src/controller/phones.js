95b0ac30b966295a53abb51d5d2d5e2e63a61af6
Your Name <you@example.com>
2023-05-27 13:49:20 +0300

    feat: add prisma

--- src/controller/phones.js
+++
@@ -1,7 +0,0 @@
-exports.getList = (req, res) => {
-  res.send([
-    { modal: "X1", year: "2001", id: 1 },
-    { modal: "X1", year: "2001", id: 2 },
-    { modal: "X1", year: "2001", id: 3 },
-  ])
-}
