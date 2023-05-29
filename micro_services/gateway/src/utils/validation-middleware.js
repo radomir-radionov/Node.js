a9e39131a1c9f291577333972487af7a790567fa
radomir-radionov <radomir.radionov26@gmail.com>
2023-05-29 11:59:23 +0300

    task13

--- micro_services/gateway/src/utils/validation-middleware.js
+++ micro_services/gateway/src/utils/validation-middleware.js
@@ -1,25 +1,25 @@
-const get = require('lodash/get')
-const { AppError } = require('./app-errors')
+const get = require("lodash/get")
+const { AppError } = require("./app-errors")
 
 module.exports = (schema) => {
-    if (!schema) {
-        throw new Error('Validation scheme is required!')
+  if (!schema) {
+    throw new Error("Validation scheme is required!")
+  }
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body)
+
+    if (error) {
+      const errorMessage = get(error, "details", []).reduce(
+        (acc, { message }) => {
+          if (message) {
+            acc += message
+          }
+          return acc
+        },
+        ""
+      )
+      return next(new AppError({ message: errorMessage, code: 400 }))
     }
-
-    return (req, res, next) => {
-        const { error } = schema.validate(req.body)
-
-        if (error) {
-            const errorMessage = get(error, 'details', [])
-                .reduce((acc, { message }) => {
-                    if (message) {
-                        acc += message
-                    }
-                    return acc
-                }, '')
-            return next(new AppError({ message: errorMessage, code: 400 }))
-        }
-
-        next()
-    }
+    next()
+  }
 }
