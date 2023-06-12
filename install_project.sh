f67dd75a000a962fee2418b4d3e6c9f962ad78d1
radomir-radionov <radomir.radionov26@gmail.com>
2023-06-12 13:07:30 +0300

    add seeds

--- install_project.sh
+++ install_project.sh
@@ -4,4 +4,7 @@ cp .env.example .env && \
 npm install && \  
 npm run docker:up && \
 npm run db:migrate && \
+npm run db:seed:up && \
 echo "Project installed and ready to run"
+
+# sleep 5 sec for waiting
---
+++ src/services/db/seeders/20230612081533-example-users.js
@@ -0,0 +1,24 @@
+/** @type {import('sequelize-cli').Migration} */
+const { faker } = require('@faker-js/faker')
+
+const tableName = 'Users'
+const moods = ['good', 'nice', 'fine']
+const getRandomArrayEl = (arr) => arr[Math.floor(Math.random() * arr.length)]
+const fakeUser = new Array(100).fill(null).map((_, i) => ({
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  email: faker.internet.email(),
+  mood: getRandomArrayEl(moods),
+  age: faker.number.int({ min: 7, max: 70 }),
+  createdAt: new Date()
+}))
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(tableName, fakeUser, {})
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete(tableName, null, {})
+  }
+}
